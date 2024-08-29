64aaf1d15bd81ca78805ddbe92635ec0f078211e
João Paulo <161873198+ojoaomc@users.noreply.github.com>
2024-08-29 20:37:35 -0300

    Create atividadeJSlista01.js

---
+++ atividadeJSlista01.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+// Cria uma interface para ler os dados do terminal
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Solicita ao usuário o primeiro número
+rl.question("Digite o primeiro número: ", (num1) => {
+  // Solicita ao usuário o segundo número
+  rl.question("Digite o segundo número: ", (num2) => {
+    // Converte as entradas para números e calcula a soma
+    const soma = parseFloat(num1) + parseFloat(num2);
+    
+    // Exibe o resultado
+    console.log(`A soma dos numeros é ${soma}`);
+    
+    // Fecha a interface
+    rl.close();
+  });
+});
