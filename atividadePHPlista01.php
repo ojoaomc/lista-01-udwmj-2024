065ad420058af2a5e3e5a1480084b472e0dbe28b
João Paulo <161873198+ojoaomc@users.noreply.github.com>
2024-08-29 22:16:05 -0300

    Create atividadePHPlista01.php

---
+++ atividadePHPlista01.php
@@ -0,0 +1,9 @@
+<?php
+
+    $num1 = readline("Informe o primeiro número: ");
+    $num2 = readline("Informe o segundo número: ");
+
+    $soma = $num1 + $num2;
+
+    echo "A soma dos números é: $soma";
+?>
